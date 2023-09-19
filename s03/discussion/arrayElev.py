67434b0cd96e8fc7758706445940917f47c0ac6a
CiteronZ <74941556+CiteronZ@users.noreply.github.com>
2023-09-19 09:18:26 +0800

    Updated // Added default cases

--- s03/discussion/arrayElev.py
+++ s03/discussion/arrayElev.py
@@ -2,13 +2,16 @@ import time
 class elev:
 
     def __init__(self, target, floors, currentf):
-        self.target = target
         self.floors = floors
         self.currentf = currentf
 
 
     def start(self):
-        move = int(input(f"What floor would you like to go to? (1-15) Current Level:{self.currentf}\n"))
+        move = int(input(f"What floor would you like to go to? Int only(1-15) Current Level:{self.currentf}\n"))
+
+        if move > 15 or move < 1:
+            print("Invalid 1-15 only")
+            elev.start(self)
 
         while self.currentf != move:
 
@@ -17,14 +20,12 @@ class elev:
                 time.sleep(1.5)
                 print("Level:",self.currentf)
 
-
             elif self.currentf > move:
                 self.currentf -= 1
                 time.sleep(1.5)
-                print("Level",self.currentf)
+                print("Level:",self.currentf)
 
         print(f"You have arrived at Level:{move}")
-
 
         again = input("Would you like to go again? y/n").lower()
 
@@ -32,7 +33,6 @@ class elev:
             case "y":
                 elev.start(self)
                 self.currentf = move
-
             case "n":
                 pass
 
