c30b6f4b7e816e2b63041d019963c565f20b8edf
Natividad, Francis BC22 <74941556+Nutt-ividad@users.noreply.github.com>
2023-10-01 16:07:17 +0800

    Update BCS22-Salary_Natividad.py

--- s02/BCS22-Salary_Natividad.py
+++ s02/BCS22-Salary_Natividad.py
@@ -14,23 +14,23 @@ class bonus_Calc:
     def calc(self):
 
         if self.Year == 5:
-            x = self.Salary * (5/100)
-            self.Final = x + self.Salary
+            x = self.Salary * (5/100) #O(1)
+            self.Final = x + self.Salary #O(1)
         elif self.Year == 10:
-            x = self.Salary * (100/100)
-            self.Final = x + self.Salary
+            x = self.Salary * (100/100) #O(1)
+            self.Final = x + self.Salary #O(1)
         elif self.Year == 15:
-            x = self.Salary * (150/100)
-            self.Final = x + self.Salary
+            x = self.Salary * (150/100) #O(1)
+            self.Final = x + self.Salary #O(1)
         elif self.Year >= 20:
-            x = self.Salary * (200/100)
-            self.Final = x + self.Salary
+            x = self.Salary * (200/100) #O(1)
+            self.Final = x + self.Salary #O(1)
         elif self.Year < 5:
-            print("Not qualified for Bonus")
+            print("Not qualified for Bonus") #O(1)
 
         self.O = 4
 
-        return print(f"The Longevity of the bonus: {x}, the final salary being: {self.Final}")
+        return print(f"The Longevity of the bonus: {x}, the final salary being: {self.Final}") #O(1)
 
     #def findO(self):
 
